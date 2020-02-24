771886ea874e47a3107445be98cf79a154a77a42
Nostee <nosteeeeeee@gmail.com>
2020-02-24 09:40:12 +0800

    removed the pycharm configuration

--- Python/Practice/Built-in Functions/SohCahToa.py
+++ Python/Practice/Built-in Functions/SohCahToa.py
@@ -1,8 +1,14 @@
 import math
 
+#length of AB (adjacent)
 len1 = int(input())
+#length of BC (opposite)
 len2 = int(input())
+
 
 print(str(int(round(math.degrees(math.atan2(len1,len2)))))+"°")
 
-print("asdfdsa")
+#atan(inverse of tan)
+#asin(inverse of sin)
+#acos(inverse of cos)
+#just add 2 after the function to receive 2 values just like the example.
