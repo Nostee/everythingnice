d66e1572bf908feae9340c05e5534f47bd988f37
Nostee <nosteeeeeee@gmail.com>
2020-03-19 11:07:06 +0800

    fixed some notes

--- Flutter/nostee_brew_crew/lib/main.dart
+++ Flutter/nostee_brew_crew/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:nostee_brew_crew/pages/wrapper.dart';
+import 'package:nostee_brew_crew/pages/authenticate.dart';
+import 'package:nostee_brew_crew/pages/home.dart';
 
 void main() => runApp(MaterialApp(
-  
+  routes: {
+    "/" : (context) => Wrapper(),
+    "/authenticate" : (context) => Authenticate(),
+    "/home" : (context) => Home(),
+
+  }
 ));
 
