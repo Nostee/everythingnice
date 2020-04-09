ce4dfeffaa041496ab5444e9dfcef06aface587b
Nostee <nosteeeeeee@gmail.com>
2020-04-09 09:30:19 +0800

    added notes

---
+++ Flutter/firebase_training/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Starter());
+
+class Starter extends StatefulWidget {
+  @override
+  _StarterState createState() => _StarterState();
+}
+
+class _StarterState extends State<Starter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
